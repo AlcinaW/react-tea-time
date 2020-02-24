50a9747716ca065a85d3a0c6661e8a21bd6186bf
awongimc <awong@imcba.com>
2020-02-24 15:10:17 -0500

    API project repos added (WIP)

--- 7-githubfinder/app.js
+++ 7-githubfinder/app.js
@@ -12,17 +12,21 @@ searchUser.addEventListener('keyup', (e) => {
     const userText = e.target.value;
 
     if (userText !== '') {
+        // make http call
         github.getUser(userText)
             .then(data => {
                 if (data.profile.message === 'Not found') {
                     // Show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
                     // Show profile
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             })
     } else {
         // Clear profile 
-
+        ui.clearProfile();
     }
 });
+
--- 7-githubfinder/github.js
+++ 7-githubfinder/github.js
@@ -2,16 +2,22 @@ class Github {
     constructor() {
         this.client_id = '';
         this.client_secret = '';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
     async getUser(user) {
         // 2 requests, one to get user and one to get repos
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profileData = await profileResponse.json();
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
             // profile in ES6+ is the same as profile: profile
-            profile
+            profile,
+            repos
         }
     }
 } 
