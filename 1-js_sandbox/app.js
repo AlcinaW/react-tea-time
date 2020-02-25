8fa4111f61d2f181e9fe3e97123c506bc7cf071b
awongimc <awong@imcba.com>
2020-02-25 11:11:37 -0500

    Character sets and quantifiers

--- 1-js_sandbox/app.js
+++ 1-js_sandbox/app.js
@@ -21,12 +21,21 @@ re = /[A-Za-z]ray/; // Match any letter
 re = /[0-9][0-9]ray/; // Match any digit 
 
 // Braces {} - Quantifiers 
+re = /He{2}o/i; // Must occur exactly {m} amount of times
+re = /He{2,4}o/i; // Must occur exactly {m} amount of times
+re = /He{2,}o/i; // Must occur at least {m} times 
+
+// Parentheses () - Grouping 
+// re = /([0-9]x){3}/;
+re = /^([0-9]x){3}$/;
 
 // String to match 
+const str = '3x3x3x';
+// const str = 'Hello';
 // const str = 'Hello World';
 // const str = 'Howollo';
 // const str = 'Xray?';
-const str = '10ray?';
+// const str = '10ray?';
 // Log results 
 const result = re.exec(str);
 console.log(result);
