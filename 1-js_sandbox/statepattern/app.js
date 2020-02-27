e39e53c8d7703bdd48453202d01a8f1e5356dc3a
awongimc <awong@imcba.com>
2020-02-27 16:29:51 -0500

    State pattern con't

--- 1-js_sandbox/statepattern/app.js
+++ 1-js_sandbox/statepattern/app.js
@@ -1,3 +1,14 @@
 const PageState = function () {
     let currentState = new homeState(this);
-}
+
+    this.init = function () {
+        this.change(new homeState);
+    }
+
+    this.change = function (state) {
+        currentState = state;
+    }
+};
+
+// Home State 
+const homeState = function (page) { } 
