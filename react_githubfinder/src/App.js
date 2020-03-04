e4df9e48d8555d6da0478fb41126c0252f8b0d0d
awongimc <awong@imcba.com>
2020-03-04 14:46:18 -0500

    Navbar component added

--- react_githubfinder/src/App.js
+++ react_githubfinder/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
-// import { render } from 'react-dom';
 
 class App extends Component {
   render() {
-    const name = 'Jesse McCree';
-    const loading = false;
-    const showName = false;
+    const numbers = [1, 2, 3, 4];
 
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
