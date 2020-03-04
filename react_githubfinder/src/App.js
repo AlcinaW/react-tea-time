a113d6bdb936c3196ef7057bb4a05f40c78e5b12
awongimc <awong@imcba.com>
2020-03-04 10:42:03 -0500

    React app file cleanup
    basic cleanup from create-react-app

--- react_githubfinder/src/App.js
+++ react_githubfinder/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = 'Jesse McCree';
+    const loading = false;
+    const showName = false;
+
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
