f01273a224ea3a4635e9ff9319bca2791531fd1f
Alcina <white_kaitou@hotmail.com>
2020-04-13 20:20:52 -0400

    Setting up GithubState

--- react_githubfinder/src/context/github/GithubState.js
+++ react_githubfinder/src/context/github/GithubState.js
@@ -1,3 +1,38 @@
-import React
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GitHubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from '../types';
 
-, { userreducer } from 'react';
+const GithubState = props => {
+    const initialState = {
+        user: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+    // Search users
+    // Get user
+    // Get repos
+    // Clear Users
+    // Set loading
+    return <GitHubContext.Provider
+        value={{
+            users: state.users,
+            user: state.user,
+            repos: state.repos,
+            loading: state.loading
+        }}
+    >
+        {props.children}
+    </GitHubContext.Provider>
+}
+
+export default GithubState
